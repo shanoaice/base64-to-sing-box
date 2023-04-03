7f5f3164d900b25b82c4ec3baf6e5f3f3ed28096
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-04-03 22:42:48 +0800

    fix(subscription): use Buffer.from instead of legacy atob

--- src/subscription.mjs
+++ src/subscription.mjs
@@ -1,26 +1,13 @@
 import got from 'got'
 import { callOrReturn } from './utils.mjs'
 
-/** this function is copied from MDN's btoa() method page, used to convert a binary string to a JavaScript UTF-16 string
- * @param {string} binary - The source binary string
- * @returns The converted UTF-16 string */
-function fromBinary(binary) {
-	const bytes = Uint8Array.from({ length: binary.length }, (element, index) =>
-		binary.charCodeAt(index),
-	)
-	const charCodes = new Uint16Array(bytes.buffer)
-
-	let result = ''
-	charCodes.forEach((char) => {
-		result += String.fromCharCode(char)
-	})
-	return result
-}
-
 function paseBase64Sub(base64) {
-	return fromBinary(atob(base64))
+	return Buffer.from(base64, 'base64')
+		.toString()
 		.split('\n')
-		.map((base64) => JSON.parse(fromBinary(atob(base64.substring(8)))))
+		.map((entry) =>
+			JSON.parse(Buffer.from(entry.substring(8), 'base64').toString()),
+		)
 }
 
 /**
@@ -36,8 +23,8 @@ function mapNetToTransportType(net) {
  * only plain non-symmetrically-encrypted QUIC is supported, seed will be ignored if specified
  * alterId (aid) > 1 will be ignored, has the same effect as alterId = 1 (legacy VMess)
  */
-export function convertBase64ToSingBox(base64Sub, extraConfig) {
-	const contents = paseBase64Sub(base64Sub)
+export function convertBase64ToSingBox(config) {
+	const contents = paseBase64Sub(config.subscription)
 	return contents
 		.map((content) =>
 			content.net !== 'kcp' // filters out KCP since this is unsupported by sing-box
@@ -46,7 +33,7 @@ export function convertBase64ToSingBox(base64Sub, extraConfig) {
 						type: 'vmess',
 						server: content.add,
 						server_port: content.port,
-						tcp_fast_open: callOrReturn(extraConfig.outbounds.tcpFastOpen),
+						tcp_fast_open: callOrReturn(config.outbounds.tcpFastOpen),
 						uuid: content.id,
 						security: content.scy || 'auto',
 						alter_id: content.aid || 0,
